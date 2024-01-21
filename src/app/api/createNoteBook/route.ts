3f66bdb6235cecda09918c8cb4080153cb037dfb
gabrimarch3 <120276758+gabrimarch3@users.noreply.github.com>
2024-01-21 20:09:55 +0100

    Integration with OpenAi API

---
+++ src/app/api/createNoteBook/route.ts
@@ -0,0 +1,16 @@
+import { generateImagePrompt } from "@/lib/openAi";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const {userId} = auth();
+    if(!userId) {
+        return new NextResponse('Non sei autenticato!', {status: 401});
+    }
+
+    const body = await req.json();
+    const {name} = body;
+    const image_description = await generateImagePrompt(name);
+    console.log({image_description});
+    return new NextResponse('ok')
+}
---
+++ src/components/Provider.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+type Props = {}
+
+const Provider = (props: Props) => {
+  return (
+    <div>Provider</div>
+  )
+}
+
+export default Provider
