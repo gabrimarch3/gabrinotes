dcdc1f2185bca6c65d4b677b706be0b81a92c0e5
gabrimarch3 <120276758+gabrimarch3@users.noreply.github.com>
2024-01-23 22:34:05 +0100

    Created a function to save the current editor

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -1,14 +1,27 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
 import TipTapMenuBar from "./TipTapMenuBar";
 import { Button } from "./ui/button";
+import { useDebounce } from "@/lib/useDebounce";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { NoteType } from "@/lib/db/schema";
 
-type Props = {};
+type Props = {note: NoteType};
 
-const TipTapEditor = (props: Props) => {
-  const [editorState, setEditorState] = useState("");
+const TipTapEditor = ({note}: Props) => {
+  const [editorState, setEditorState] = useState(note.editorState || "");
+  const saveNote = useMutation({
+    mutationFn: async () => {
+        const response = await axios.post('/api/saveNote', {
+            noteId: note.id,
+            editorState
+        })
+        return response.data;
+    }
+  })
 
   const editor = useEditor({
     autofocus: true,
@@ -19,11 +32,27 @@ const TipTapEditor = (props: Props) => {
     },
   });
 
+  const debounceEditorState = useDebounce(editorState, 300);
+
+  useEffect(() => {
+    if(debounceEditorState === '') return;
+    saveNote.mutate(undefined, {
+        onSuccess: data => {
+            console.log('aggiornato con successo!', data)
+        },
+        onError: error => {
+            console.error('Aggiornamento non riuscito! Riprovare', error)
+        }
+    })
+  }, [debounceEditorState])
+
   return (
     <>
       <div className="flex">
         {editor && <TipTapMenuBar editor={editor}/>}
-        <Button>Salvato</Button>
+        <Button disabled variant='outline'>
+            {saveNote.isPending? "Salvando.." : "Salvato"}
+        </Button>
       </div>
       <div className="prose">
         <EditorContent editor={editor} />
